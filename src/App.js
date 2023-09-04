648df6b697744cfd683bd3b59e097a7bcbbc8855
Alex Korbutovski <akorbutovski@elinext.com>
2023-09-04 08:01:45 +0300

    create: useToggle hook

--- src/App.js
+++ src/App.js
@@ -1,52 +1,9 @@
 import './App.css';
 import {useToggle} from "./useToggle";
-// import {useReducer} from "react";
-
-// const initState = {
-//     color: 'white',
-// }
-
-// const reducer = (state, action) => {
-//     console.log('action: ', action)
-//     switch (action.type) {
-//         case 0:
-//             return {
-//                 ...state,
-//                 count: state.count + action.payload
-//             }
-//         case 1:
-//             return {
-//                 ...state,
-//                 count: state.count - action.payload
-//             }
-//         case  'RESET':
-//             return {
-//                 ...state,
-//                 count: 0
-//             }
-//         case  'CHANGE':
-//             return {
-//                 ...state,
-//                 name: action.payload
-//             }
-//         default:
-//             throw new Error()
-//     }
-// }
 
 function App() {
     const [value, toggle] = useToggle(['blue', 'orange', 'cyan', 'teal']);
     // const [value, toggle] = useToggle(['light', 'dark']);
-
-    // const [state, dispatchColor] = useReducer(reducer, initState);
-    // console.log('state: ', state)
-
-    // const dispatchColor = () => {
-    //     dispatch({
-    //         type: 'DESC',
-    //         payload: 10
-    //     });
-    // }
 
     return (
         <button onClick={() => toggle('dark')}>
